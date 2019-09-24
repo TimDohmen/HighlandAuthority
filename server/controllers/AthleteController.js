351ae8181cc61f599ab0f6cf0fb9653472023c69
Zach Pfeifer <mr.zman1@yahoo.com>
2019-09-24 13:46:50 -0600

    AthleteController done

---
+++ server/controllers/AthleteController.js
@@ -0,0 +1,62 @@
+import AthleteService from '../services/AthleteService'
+import express from 'express'
+import { Authorize } from '../middleware/authorize.js'
+
+let _as = new AthleteService().repository
+
+export default class AthleteController {
+  constructor() {
+    this.router = express.Router()
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .use(Authorize.authenticated)
+      .post('', this.create)
+      .put('/:id', this.edit)
+    // .delete('/id', this.delete)
+
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await _as.find({})
+      return res.send(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await _as.findOne({ _id: req.params.id })
+      return res.send(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async create(req, res, next) {
+    try {
+      let data = await _as.create(req.body)
+      return res.send(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async edit(req, res, next) {
+    try {
+      let data = await _as.findOneAndUpdate({ _id: req.params.id, userId: req.session.uid }, req.body, { new: true })
+      return res.send(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // async delete(req, res, next) {
+  //   try {
+  //     let data = await _as.findOneAndRemove({ _id: req.params.id, userId: req.session.uid })
+  //     if (!data) {
+  //       throw new Error("invalid id, you didn't say the magic word")
+  //     } res.send("deleted")
+  //   } catch (error) { next(error) }
+
+  // }
+}
