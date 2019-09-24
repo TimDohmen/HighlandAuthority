672f2be78ae42e0093faa58acb88b92169d208ea
Andrew Gruver <gr82cu@gmail.com>
2019-09-24 13:45:39 -0600

    Made Game controller and service

--- server/controllers/GameController.js
+++ server/controllers/GameController.js
@@ -1,11 +1,11 @@
-import BoardService from '../services/BoardService'
+import GameService from '../services/GameServices'
 import express from 'express'
 import { Authorize } from '../middleware/authorize.js'
 
-let _boardService = new BoardService().repository
+let _gameService = new GameService().repository
 
 //PUBLIC
-export default class BoardsController {
+export default class GameController {
   constructor() {
     this.router = express.Router()
       .use(Authorize.authenticated)
@@ -25,8 +25,7 @@ export default class BoardsController {
 
   async getAll(req, res, next) {
     try {
-      //only gets boards by user who is logged in
-      let data = await _boardService.find({ authorId: req.session.uid })
+      let data = await _gameService.find({ userId: req.session.uid })
       return res.send(data)
     }
     catch (err) { next(err) }
@@ -34,22 +33,22 @@ export default class BoardsController {
 
   async getById(req, res, next) {
     try {
-      let data = await _boardService.findOne({ _id: req.params.id, authorId: req.session.uid })
+      let data = await _gameService.findOne({ _id: req.params.id, userId: req.session.uid })
       return res.send(data)
     } catch (error) { next(error) }
   }
 
   async create(req, res, next) {
     try {
-      req.body.authorId = req.session.uid
-      let data = await _boardService.create(req.body)
+      req.body.userId = req.session.uid
+      let data = await _gameService.create(req.body)
       return res.status(201).send(data)
     } catch (error) { next(error) }
   }
 
   async edit(req, res, next) {
     try {
-      let data = await _boardService.findOneAndUpdate({ _id: req.params.id, authorId: req.session.uid }, req.body, { new: true })
+      let data = await _gameService.findOneAndUpdate({ _id: req.params.id, userId: req.session.uid }, req.body, { new: true })
       if (data) {
         return res.send(data)
       }
@@ -59,7 +58,7 @@ export default class BoardsController {
 
   async delete(req, res, next) {
     try {
-      await _boardService.findOneAndRemove({ _id: req.params.id, authorId: req.session.uid })
+      await _gameService.findOneAndRemove({ _id: req.params.id, userId: req.session.uid })
       return res.send("Successfully deleted")
     } catch (error) { next(error) }
   }
