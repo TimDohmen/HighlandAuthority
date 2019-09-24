a89b157ebb9b2160693e1d0faa5ad944daa4b562
Zach Pfeifer <mr.zman1@yahoo.com>
2019-09-24 15:18:34 -0600

    Started Profile Vue

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="board">{{board.title}}</div>
+  <div class="profile">{{profile.title}}</div>
 </template>
 
 <script>
 export default {
-  name: "board",
+  name: "profile",
   computed: {
-    board() {
+    profile() {
       return (
         //FIXME This does not work on page reload because the boards array is empty in the store
         this.$store.state.boards.find(b => b._id == this.boardId) || {
