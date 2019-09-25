9a65b4fd789d7743247885229b617f6ee63c1a50
Josiah Aubin <josiahaubin@gmail.com>
2019-09-25 15:50:18 -0600

    Mobile profile edit

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -2,7 +2,7 @@
   <div class="profile container-fluid">
     <!-- Profile Card -->
     <div class="row">
-      <div class="card w-25 text-left">
+      <div class="card col-sm-12 col-md-3 text-left">
         <img :src="profile.picture" class="card-img-top border rounded m-auto img-fluid" />
         <div class="card-body">
           <h3>{{user.name}}</h3>
@@ -25,13 +25,13 @@
     <!-- Start of Bio/Location/Phone -->
     <hr />
     <div class="row">
-      <div class="col-4">
+      <div class="col-sm-12 col-md-4">
         <h4>Bio: {{profile.bio}}</h4>
       </div>
-      <div class="col-4">
+      <div class="col-sm-12 col-md-4">
         <h4>Location: {{profile.location}}</h4>
       </div>
-      <div class="col-4">
+      <div class="col-sm-12 col-md-4">
         <h4>Phone #: {{profile.phone}}</h4>
       </div>
     </div>
