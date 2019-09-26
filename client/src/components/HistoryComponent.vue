f41937935d7894184b9c3ecd8c7e84a5b577c1b9
Tim Dohmen <dohmentim@yahoo.com>
2019-09-26 09:42:32 -0600

    history linked

---
+++ client/src/components/HistoryComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="history">hello</div>
+</template>
+
+
+<script>
+export default {
+  name: "history",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
