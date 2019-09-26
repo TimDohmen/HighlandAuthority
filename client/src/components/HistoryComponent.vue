3d70a3cb94c8170b2c520b50993f27448a448cd2
Tim Dohmen <dohmentim@yahoo.com>
2019-09-26 10:50:34 -0600

    onclick history renders

--- client/src/components/HistoryComponent.vue
+++ client/src/components/HistoryComponent.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="history">hello</div>
+  <div class="history">
+    <historyScore v-for="score in scores" :scoreProp="score" :key="score._id" />
+  </div>
 </template>
 
 
 <script>
+import historyScore from "../components/HistoryScoreComponent";
 export default {
   name: "history",
-  mounted() {    
-  },
+  mounted() {},
   data() {
     return {};
   },
   computed: {
-scores(){
-  return this.$store.state.oneEventScores
-}
+    scores() {
+      return this.$store.state.oneEventScores;
+    }
   },
   methods: {},
-  components: {}
+  components: { historyScore }
 };
 </script>
 
---
+++ client/src/components/HistoryScoreComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="historyScore">
+    <p>{{scoreProp.topAttempt}}</p>
+    <p>{{scoreProp.createdAt}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "historyScore",
+  data() {
+    return {};
+  },
+  props: ["scoreProp"],
+  computed: {
+    scores() {
+      return this.$store.state.oneEventScores;
+    }
+  },
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
