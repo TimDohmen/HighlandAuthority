fa16dd3a851eb4d318a294c205ffede62c2e8e75
Josiah Aubin <josiahaubin@gmail.com>
2019-09-30 10:10:23 -0600

    Games creator simple design

--- client/src/views/Games.vue
+++ client/src/views/Games.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="games"></div>
+  <div class="games container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1>Games Creator</h1>
+        <form>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Enter Name of the Games" />
+          </div>
+          <div class="form-group">
+            <label for="location">Location</label>
+            <input type="text" class="form-control" id="location" placeholder="Games Location" />
+          </div>
+          <div class="form-group">
+            <label for="date">Date</label>
+            <input type="date" class="form-control" id="date" placeholder="Games Date" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -17,4 +38,10 @@ export default {
 
 
 <style scoped>
+form {
+  background-color: rgba(255, 255, 255, 0.8);
+}
+.form-group {
+  text-align: left;
+}
 </style>
