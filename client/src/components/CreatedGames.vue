f264e12e8e86db43022b13a0ad7eab671bf2f58a
Josiah Aubin <josiahaubin@gmail.com>
2019-09-30 15:05:49 -0600

    Games showing up

---
+++ client/src/components/CreatedGames.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="CreatedGames">
+    <div class="col-4">
+      <h1>{{gameProp.name}}</h1>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "CreatedGames",
+  props: ["gameProp"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
