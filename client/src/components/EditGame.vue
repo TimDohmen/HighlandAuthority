d30c8ffdd71c6ed9bc77dd0b229ef077d5fef8bb
Josiah Aubin <josiahaubin@gmail.com>
2019-09-30 16:23:24 -0600

    Edit game modal created

---
+++ client/src/components/EditGame.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="EditGame">
+    <div id="Edit-Game-Modal" class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div class="modal-title">
+              <h1>
+                Edit Game
+                <button data-dismiss="modal" class="btn btn-outline-danger xButton">x</button>
+              </h1>
+            </div>
+          </div>
+          <div class="modal-body">
+            <form @submit.prevent="editProfile()">
+              <div class="form-group">
+                <label for="name">Name:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  placeholder="Enter Name"
+                  v-model="newEdit.name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="location">Location:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="location"
+                  placeholder="Enter Location"
+                  v-model="newEdit.location"
+                />
+              </div>
+              <div class="form-group">
+                <label for="date">Date :</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="date"
+                  placeholder="Enter Date"
+                  v-model="newEdit.date"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+          <!-- end of modal body -->
+          <div class="modal-footer">
+            <button
+              id="close"
+              type="button"
+              class="btn btn-outline-danger"
+              data-dismiss="modal"
+            >Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "EditGame",
+  data() {
+    return {
+      newEdit: {}
+    };
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+.form-group {
+  text-align: left;
+}
+</style>
