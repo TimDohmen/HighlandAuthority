cbc390a68db0131d2369dac5ceee002912ccac23
Zach Pfeifer <mr.zman1@yahoo.com>
2019-10-02 10:52:05 -0600

    profile card

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -2,8 +2,8 @@
   <div class="profile container-fluid db-border">
     <!-- Profile Card -->
     <div class="row">
-      <div class="card col-sm-12 col-md-3 text-left db-border m-2">
-        <img :src="profile.picture" class="card-img-top border rounded m-auto img-fluid db-border" />
+      <!-- <div class="card col-sm-12 col-md-3 text-left db-border m-2"> -->
+      <!-- <img :src="profile.picture" class="card-img-top border rounded m-auto img-fluid db-border" />
         <div class="card-body db-border">
           <h3 class="displayRole">{{user.name}}</h3>
           <i v-if="user.role == 'Admin'" class="ra ra-shield ra-2x ml-1"></i>
@@ -11,16 +11,41 @@
           <i v-if="user.role == 'Judge'" class="ra ra-gavel ra-2x ml-1"></i>
           <h6>Nickname: {{profile.nickname}}</h6>
           <h6>Class: {{profile.class}}</h6>
-        </div>
-        <button
-          class="btn btn-warning m-2"
-          data-toggle="modal"
-          data-target="#Edit-Profile-Modal"
-        >Edit</button>
-        <router-link v-if="user.role!='Athlete'" to="/games" class="btn btn-light mb-2">Admin Page</router-link>
+      </div>-->
 
-        <EditProfileModal />
-      </div>
+      <!-- FIXME Construction -->
+      <figure class="snip1515">
+        <div class="profile-image">
+          <img
+            :src="profile.picture"
+            class="card-img-top border rounded m-auto img-fluid db-border"
+          />
+        </div>
+        <figcaption>
+          <h3 class="displayRole">{{user.name}}</h3>
+          <i v-if="user.role == 'Admin'" class="ra ra-shield ra-2x ml-1"></i>
+          <i v-if="user.role == 'Athlete'" class="ra ra-muscle-fat ra-2x ml-1"></i>
+          <i v-if="user.role == 'Judge'" class="ra ra-gavel ra-2x ml-1"></i>
+          <h6>Nickname: {{profile.nickname}}</h6>
+          <h6>Class: {{profile.class}}</h6>
+          <div class="icons">
+            <button
+              class="btn btn-warning m-2"
+              data-toggle="modal"
+              data-target="#Edit-Profile-Modal"
+            >Edit</button>
+            <router-link
+              v-if="user.role!='Athlete'"
+              to="/games"
+              class="btn btn-light mb-2"
+            >Admin Page</router-link>
+          </div>
+        </figcaption>
+      </figure>
+      <!-- //FIXME END OF CONSTRUCTION -->
+
+      <EditProfileModal />
+      <!-- </div> -->
       <!-- Event/PR area -->
       <PR />
       <history />
@@ -83,5 +108,91 @@ img {
 }
 .displayRole {
   display: inline;
+}
+
+.snip1515 {
+  font-family: "Open Sans", Arial, sans-serif;
+  position: relative;
+  margin: 10px;
+  min-width: 230px;
+  max-width: 315px;
+  width: 100%;
+  color: #000000;
+  text-align: center;
+  line-height: 1.4em;
+  font-size: 14px;
+  box-shadow: none !important;
+}
+
+.snip1515 * {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.snip1515 .profile-image {
+  display: inline-block;
+  width: 80%;
+  z-index: 1;
+  position: relative;
+  padding: 10px;
+  border: 2px solid #e8b563;
+}
+
+.snip1515 .profile-image img {
+  max-width: 100%;
+  vertical-align: top;
+}
+
+.snip1515 figcaption {
+  width: 100%;
+  background-color: #f2f2f2;
+  color: #555;
+  padding: 125px 25px 25px;
+  margin-top: -100px;
+  display: inline-block;
+}
+
+.snip1515 h3,
+.snip1515 h4,
+.snip1515 p {
+  margin: 0 0 5px;
+}
+
+.snip1515 h3 {
+  font-weight: 600;
+  font-size: 1.3em;
+  font-family: "Playfair Display", Arial, sans-serif;
+}
+
+.snip1515 h4 {
+  color: #8c8c8c;
+  font-weight: 400;
+  letter-spacing: 2px;
+}
+
+.snip1515 p {
+  font-size: 0.9em;
+  letter-spacing: 1px;
+  opacity: 0.9;
+}
+
+.snip1515 .icons {
+  text-align: center;
+  width: 100%;
+}
+
+.snip1515 i {
+  padding: 10px 2px;
+  display: inline-block;
+  font-size: 18px;
+  font-weight: normal;
+  color: #e8b563;
+  opacity: 0.75;
+}
+
+.snip1515 i:hover {
+  opacity: 1;
+  -webkit-transition: all 0.35s ease;
+  transition: all 0.35s ease;
 }
 </style>
